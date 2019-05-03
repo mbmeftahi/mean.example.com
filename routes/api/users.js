51e7587bcd71cc79770d00f6c65d39c5abe37900
Mohammad B Meftahi <mmeftahi@hotmail.com>
2019-05-03 11:59:48 -0500

    create new user record

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var Users = require('../../models/users');
 
+// get all users
 router.get('/', function(req, res, next) {
   Users.find({},function(err, users){
     if(err){
@@ -23,5 +24,24 @@ router.get('/:userId', function(req,res){
        return res.json({'success':true, 'user': user});
      });
    });
+   
+   // post/update option
+   router.post('/', function(req, res) {
+    Users.create(new Users({
+      username: req.body.username,
+      email: req.body.email,
+      first_name: req.body.first_name,
+      last_name: req.body.last_name
+    }), function(err, user){
+      
+      if(err){
+        return res.json({success: false, user: req.body, error: err});
+      }
+  
+      return res.json({success: true, user: user});
+      
+    });
+    });
+
 
 module.exports = router;
