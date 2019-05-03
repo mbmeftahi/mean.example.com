dafc354f69df219e1478180e453c417922700792
Mohammad B Meftahi <mmeftahi@hotmail.com>
2019-05-03 09:53:29 -0500

    created a sitewide configuration file

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+var config = require('./config.dev');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -39,3 +40,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//router.get('/', function(req, res, next) {
+//  res.render('index', { title: 'Express', name: 'MBMeftahi'});
+//});
+
+router.get('/', function(req, res) {
+  res.send('respond with a resource');
+  //console.log(config);
 });
 
 module.exports = router;
